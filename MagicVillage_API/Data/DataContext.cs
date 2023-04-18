48d50e735097062e0314cfc3ffd305a786054ab3
Maycon Jardim <mayconsmill@gmail.com>
2023-04-18 10:32:00 -0300

    Instalando EF, Configurando SQL e criando DataContext.

---
+++ MagicVillage_API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using MagicVillage_API.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace MagicVillage_API.Data
+{ 
+    public class DataContext : DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+
+        public DbSet<Villa> Villas { get; set; }
+    }
+}
--- MagicVillage_API/Program.cs
+++ MagicVillage_API/Program.cs
@@ -1,12 +1,19 @@
+using MagicVillage_API.Data;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 var builder = WebApplication.CreateBuilder(args);
 
 Log.Logger = new LoggerConfiguration().MinimumLevel.Debug()
     .WriteTo.File("log/villaLogs.txt", rollingInterval: RollingInterval.Day).CreateLogger();
 
+
+
 builder.Host.UseSerilog();
 // Add services to the container.
+
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer
+         (builder.Configuration.GetConnectionString("Database")));
 
 builder.Services.AddControllers().AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
